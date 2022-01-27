b338bdc060be043aee8134467b05320b9250891f
AloeSwap <info@aloeswap.com>
2022-01-28 00:27:44 +0100

    New

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,12 +7,12 @@ module.exports = async function(deployer) {
  const nftSymbol = 'PIG';
  const productTokenName = 'Truffle';
  const productTokenSymbol = 'TRF';
- //const productToken = ProductToken.at('');
+ const productToken = ProductToken.at('0x56070F3141fd0c858307Df2d29De17467Be31Ef2');
  //const nft = NFT.at('0x0000000000000000000000000000000000000000');
 
- //PRODUCT TOKEN:
- await deployer.deploy(ProductToken, productTokenName, productTokenSymbol);
- const productToken = await NFT.deployed();
+ // PRODUCT TOKEN:
+ //await deployer.deploy(ProductToken, productTokenName, productTokenSymbol);
+ //const productToken = await NFT.deployed();
 
  // NFT:
  await deployer.deploy(NFT, nftName, nftSymbol, productToken.address);
