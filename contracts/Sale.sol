007db18addaaf9b2904677db88825e96cd0aa567
AloeSwap <info@aloeswap.com>
2022-01-29 22:03:25 +0100

    Sale contract

--- contracts/Sale.sol
+++ contracts/Sale.sol
@@ -14,9 +14,10 @@ contract Sale is Ownable, ReentrancyGuard {
     using SafeERC20Mint for IERC20Mint;
     IERC20 currency;
     address devAddress;
-    mapping (address => Details) public tokens;
+    Details[] tokens;
+    uint buyTotal;
     event eventBuy(address indexed tokenAddress, uint indexed amountCurrency, uint indexed amountOur);
-    event eventAddToken(address indexed tokenAddress, Details indexed details);
+    event eventAddToken(uint indexed id, Details indexed details);
 
     constructor(address _currencyAddress) {
         currency = IERC20(_currencyAddress);
@@ -24,28 +25,30 @@ contract Sale is Ownable, ReentrancyGuard {
     }
 
     struct Details {
+        address tokenAddress;
         uint initialPrice;
         uint increaseEvery;
         uint multiplier;
-        uint sold;
+        uint buyAmount;
+        uint buyCount;
     }
 
-    function buy(address _tokenAddress, uint _amount) public nonReentrant {
-        require(tokens[_tokenAddress].initialPrice > 0, 'buy: Token not found'); // TODO: check if token exists more elegantly
+    function buyToken(uint _id, uint _amount) public nonReentrant {
+        require(_id <= tokens.length, 'buy: Token not found');
         require(currency.allowance(msg.sender, address(this)) >= _amount, 'buy: Currency allowance is too low');
 
-        uint sold = tokens[_tokenAddress].sold;
-        uint price = tokens[_tokenAddress].initialPrice;
-        uint incEvery = tokens[_tokenAddress].increaseEvery;
-        uint multiplier = tokens[_tokenAddress].multiplier;
+        uint buy = tokens[_id].buyAmount;
+        uint price = tokens[_id].initialPrice;
+        uint incEvery = tokens[_id].increaseEvery;
+        uint multiplier = tokens[_id].multiplier;
         uint amountOur;
-        uint segmentNum = sold/incEvery;
+        uint segmentNum = buy / incEvery;
         uint priceActual = price;
         for (uint i = 1; i < segmentNum; i++) {
-            priceActual = priceActual + priceActual * multiplier / 10000;
+            priceActual += priceActual * multiplier / 10000;
         }
         uint actualAmount = _amount;
-        uint segmentCount = incEvery - (sold % incEvery);
+        uint segmentCount = incEvery - (buy % incEvery);
         while (actualAmount > 0) {
             uint segmentCost = segmentCount * priceActual;
             if (actualAmount < segmentCost) {
@@ -61,18 +64,19 @@ contract Sale is Ownable, ReentrancyGuard {
 
         currency.safeTransferFrom(msg.sender, address(this), _amount);
         currency.safeTransfer(devAddress, _amount);
-        IERC20Mint token = IERC20Mint(_tokenAddress);
+        IERC20Mint token = IERC20Mint(tokens[_id].tokenAddress);
         token.mint(amountOur);
         token.safeTransfer(msg.sender, amountOur);
-        tokens[_tokenAddress].sold += amountOur;
-        emit eventBuy(_tokenAddress, _amount, amountOur);
+        tokens[_id].buyAmount += amountOur;
+        tokens[_id].buyCount++;
+        buyTotal++;
+        emit eventBuy(tokens[_id].tokenAddress, _amount, amountOur);
     }
 
     function addToken(address _tokenAddress, uint _initialPrice, uint _increaseEvery, uint _multiplier) public onlyOwner {
-        // TODO: require(tokens[_tokenAddress] == address(0x0), 'addToken: Token not found');
-        Details memory details = Details(_initialPrice, _increaseEvery, _multiplier, 0);
-        tokens[_tokenAddress] = details;
-        emit eventAddToken(_tokenAddress, details);
+        Details memory details = Details(_tokenAddress, _initialPrice, _increaseEvery, _multiplier, 0, 0);
+        tokens.push(details);
+        emit eventAddToken(tokens.length - 1, details);
     }
 
     function setDevAddress(address _devAddress) public onlyOwner {
