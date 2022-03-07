2e8eccedd47a2626ce46ae6ed8390f940692eccd
Cezarry <pawiano8@gmail.com>
2022-03-07 11:37:57 +0100

    Added allowUnlimitedContractSize

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,44 +1,47 @@
-import { HardhatUserConfig, task } from 'hardhat/config';
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
 
-const fs = require('fs');
-const mnemonic = fs.readFileSync('.secret').toString().trim();
-const api_key_bscscan = fs.readFileSync('.apikey_bscscan').toString().trim();
-const api_key_polygonscan = fs.readFileSync('.apikey_polygonscan').toString().trim();
-const api_key_avax = fs.readFileSync('.apikey_avax').toString().trim();
-const api_key_optimism = fs.readFileSync('.apikey_optimism').toString().trim();
-const api_key_fantom = fs.readFileSync('.apikey_ftmscan').toString().trim();
-const api_key_hecoinfo = fs.readFileSync('.apikey_hecoinfo').toString().trim();
-const api_key_cronos = fs.readFileSync('.apikey_cronos').toString().trim();
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const api_key_bscscan = fs.readFileSync(".apikey_bscscan").toString().trim();
+const api_key_polygonscan = fs.readFileSync(".apikey_polygonscan").toString().trim();
+const api_key_avax = fs.readFileSync(".apikey_avax").toString().trim();
+const api_key_optimism = fs.readFileSync(".apikey_optimism").toString().trim();
+const api_key_fantom = fs.readFileSync(".apikey_ftmscan").toString().trim();
+const api_key_hecoinfo = fs.readFileSync(".apikey_hecoinfo").toString().trim();
+const api_key_cronos = fs.readFileSync(".apikey_cronos").toString().trim();
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
   for (const account of accounts) console.log(account.address);
 });
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.11',
+    version: "0.8.11",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
     polygonTestnet: {
       //url: 'https://rpc-mumbai.matic.today',
-      url: 'https://matic-mumbai.chainstacklabs.com',
+      url: "https://matic-mumbai.chainstacklabs.com",
       //url: 'https://rpc-mumbai.maticvigil.com',
       //url: 'https://matic-testnet-archive-rpc.bwarelabs.com',
       chainId: 80001,
       accounts: { mnemonic: mnemonic },
     },
     polygonMainnet: {
-      url: 'https://polygon-rpc.com',
+      url: "https://polygon-rpc.com",
       //url: 'https://rpc-mainnet.matic.network',
       //url: 'https://matic-mainnet.chainstacklabs.com',
       //url: 'https://rpc-mainnet.maticvigil.com',
@@ -48,62 +51,62 @@ const config: HardhatUserConfig = {
       accounts: { mnemonic: mnemonic },
     },
     bscTestnet: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       accounts: { mnemonic: mnemonic },
     },
     bscMainnet: {
-      url: 'https://bsc-dataseed2.binance.org',
+      url: "https://bsc-dataseed2.binance.org",
       chainId: 56,
       accounts: { mnemonic: mnemonic },
     },
     avaxTestnet: {
-      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
       chainId: 43113,
       accounts: { mnemonic: mnemonic },
     },
     avaxMainnet: {
-      url: 'https://api.avax.network/ext/bc/C/rpc',
+      url: "https://api.avax.network/ext/bc/C/rpc",
       chainId: 43114,
       accounts: { mnemonic: mnemonic },
     },
     optimismTestnet: {
-      url: 'https://kovan.optimism.io/',
+      url: "https://kovan.optimism.io/",
       chainId: 69,
       accounts: { mnemonic: mnemonic },
     },
     optimismMainnet: {
-      url: 'https://mainnet.optimism.io',
+      url: "https://mainnet.optimism.io",
       chainId: 10,
       accounts: { mnemonic: mnemonic },
     },
     fantomTestnet: {
-      url: 'https://rpc.testnet.fantom.network/',
+      url: "https://rpc.testnet.fantom.network/",
       chainId: 4002,
       accounts: { mnemonic: mnemonic },
     },
     fantomMainnet: {
-      url: 'https://rpc.ftm.tools/',
+      url: "https://rpc.ftm.tools/",
       chainId: 250,
       accounts: { mnemonic: mnemonic },
     },
     hecoTestnet: {
-      url: 'https://http-testnet.hecochain.com/',
+      url: "https://http-testnet.hecochain.com/",
       chainId: 256,
       accounts: { mnemonic: mnemonic },
     },
     hecoMainnet: {
-      url: 'https://http-mainnet.hecochain.com/',
+      url: "https://http-mainnet.hecochain.com/",
       chainId: 128,
       accounts: { mnemonic: mnemonic },
     },
     cronosTestnet: {
-      url: 'https://cronos-testnet-3.crypto.org:8545',
+      url: "https://cronos-testnet-3.crypto.org:8545",
       chainId: 338,
       accounts: { mnemonic: mnemonic },
     },
     cronosMainnet: {
-      url: 'https://evm-cronos.org',
+      url: "https://evm-cronos.org",
       chainId: 25,
       accounts: { mnemonic: mnemonic },
     },
